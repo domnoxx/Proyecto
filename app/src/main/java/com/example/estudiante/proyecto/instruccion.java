1ae15ad57ce06c4aff57673a88259ac903ee6822
domnoxx <d-avis89@hotmail.es>
2016-07-01 13:34:12 -0500

    pantallas

--- app/src/main/java/com/example/estudiante/proyecto/instruccion.java
+++ app/src/main/java/com/example/estudiante/proyecto/instruccion.java
@@ -1,8 +1,10 @@
 package com.example.estudiante.proyecto;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -15,5 +17,9 @@ public class instruccion extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_instruccion);
+    }
+    public void volver(View v){
+        Intent i= new Intent(this,menu.class);
+        startActivity(i);
     }
 }
