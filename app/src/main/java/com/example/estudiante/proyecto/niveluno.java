6a66679857cb7af499ce4da6c0e4755bd80774b6
domnoxx <d-avis89@hotmail.es>
2016-07-01 13:41:53 -0500

    pantallas

--- app/src/main/java/com/example/estudiante/proyecto/niveluno.java
+++ app/src/main/java/com/example/estudiante/proyecto/niveluno.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class juego extends AppCompatActivity {
+public class niveluno extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +14,6 @@ public class juego extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        setContentView(R.layout.activity_juego);
+        setContentView(R.layout.activity_niveluno);
     }
 }
